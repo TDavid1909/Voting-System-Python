f30704a398255fcfcc9211b033a9375daf038f78
David <datora19@gmail.com>
2022-10-20 14:24:41 -0400

    This is a trie of the changes made

--- base_code.py
+++ base_code.py
@@ -1,17 +1,1 @@
-from ast import AnnAssign
-from dataclasses import asdict
-from re import A
-from socket import ALG_SET_AEAD_ASSOCLEN
-from urllib.request import AbstractDigestAuthHandler
-
-
-asdasdasx.difference(y)
-    ALG_SET_AEAD_ASSOCLEasd\asd\
-        
-        AbstractDigestAuthHandlerasx.difference(y)asdict
-        asdictasd
-        asdict
-        AnnAssign.discard()sda
-        sd
-        ad
-        s
+#main votaciones
