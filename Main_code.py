479caba64e9f3e3e71d2126c24beb14cf6c0c845
TDavid1909 <111788629+TDavid1909@users.noreply.github.com>
2022-10-20 16:00:27 -0400

    This is the progress so far

--- Main_code.py
+++ Main_code.py
@@ -1,11 +1,90 @@
 #this is could be the main terminal but guys feel free if you want to create more
+
+
 voter_id = [1,2,3,4,5,6,7,8]
 
 number_of_voter = len(voter_id)
+
+
 
 while True:
     voter = int(input("Enter your voter id: "))
     if voter in voter_id:
         print("You are a voter")
         voter_id.remove(voter)
-    
+        print("-------------------")
+        print("To vote for repulicans press R")
+        print("-------------------")
+        print("To vote for Democrats press D")
+        print("-------------------")
+        print("To vote for other party press O")
+        choise = input("Your choise: ")
+        choise = choise.upper(choise)
+        if choise == "R":
+            print("You want to vote for the Governorship press P")
+            print("You want to vote for the Senate press S")
+            print("You want to vote for the House press H")
+            print("You want to vote for the Governorship press G")
+            house = input("Your option: ")
+            house = house.upper(house)
+            if house == "S":
+                print("Thank you for your vote")
+            elif house == "P":
+                print("Thank you for your vote")
+            elif house == "H":
+                print("Thank you for your vote")
+            elif house == "G":
+                print("Thank you for your vote")
+        elif choise == "D":
+             print("-------------------")
+        print("To vote for repulicans press R")
+        print("-------------------")
+        print("To vote for Democrats press D")
+        print("-------------------")
+        print("To vote for other party press O")
+        choise = input("Your choise: ")
+        choise = choise.upper(choise)
+        if choise == "R":
+            print("You want to vote for the Governorship press P")
+            print("You want to vote for the Senate press S")
+            print("You want to vote for the House press H")
+            print("You want to vote for the Governorship press G")
+            house = input("Your option: ")
+            house = house.upper(house)
+            if house == "S":
+                print("Thank you for your vote")
+            elif house == "P":
+                print("Thank you for your vote")
+            elif house == "H":
+                print("Thank you for your vote")
+            elif house == "G":
+                print("Thank you for your vote")
+        elif choise == "O":
+             print("-------------------")
+        print("To vote for repulicans press R")
+        print("-------------------")
+        print("To vote for Democrats press D")
+        print("-------------------")
+        print("To vote for other party press O")
+        choise = input("Your choise: ")
+        choise = choise.upper(choise)
+        if choise == "R":
+            print("You want to vote for the Governorship press P")
+            print("You want to vote for the Senate press S")
+            print("You want to vote for the House press H")
+            print("You want to vote for the Governorship press G")
+            house = input("Your option: ")
+            house = house.upper(house)
+            if house == "S":
+                print("Thank you for your vote")
+            elif house == "P":
+                print("Thank you for your vote")
+            elif house == "H":
+                print("Thank you for your vote")
+            elif house == "G":
+                print("Thank you for your vote")    
+            
+            
+    else:
+        print("You've already voted")
+        
