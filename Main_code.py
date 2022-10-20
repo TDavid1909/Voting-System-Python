c83aa5f7c7530991be969079669286d143b30713
TDavid1909 <111788629+TDavid1909@users.noreply.github.com>
2022-10-20 15:39:43 -0400

    Twice vote system

--- Main_code.py
+++ Main_code.py
@@ -1,15 +1,11 @@
 #this is could be the main terminal but guys feel free if you want to create more
-name = ["David","Yihad"]
+voter_id = [1,2,3,4,5,6,7,8]
+
+number_of_voter = len(voter_id)
+
 while True:
-    enter = input("What's your name?")
-    name.append(enter)
-    if name == enter:
-        print("Sorry you have already vote")
-    else:
-        print("You can vote")
-        break
-   
-https://prod.liveshare.vsengsaas.visualstudio.com/join?BCA92FE08A78FB565BC48F90B3ACD47B0E8E    
-    
-   
+    voter = int(input("Enter your voter id: "))
+    if voter in voter_id:
+        print("You are a voter")
+        voter_id.remove(voter)
     
