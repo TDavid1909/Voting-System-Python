78f73a931d31f6cafac801f49f10d5bf32ada822
TDavid1909 <111788629+TDavid1909@users.noreply.github.com>
2022-10-20 15:09:34 -0400

    Update Main_code.py

--- Main_code.py
+++ Main_code.py
@@ -1,10 +1,9 @@
 #this is could be the main terminal but guys feel free if you want to create more
-voting_list = True
-already_voted = []
-while voting_list:
-    print("Enter your name")
-    next_name = input()
-    if next_name in already_voted:
-        print("You cannot vote again")
-    else:
-        already_voted.append(next_name)
+
+name = []
+while True:
+    enter = input("What's your name?")
+    name.append(enter)
+    
+   
+    
