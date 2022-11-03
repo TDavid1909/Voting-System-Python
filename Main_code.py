9af44a6c8a25cf96ff13677bf7c55daa8f791d54
TDavid1909 <111788629+TDavid1909@users.noreply.github.com>
2022-11-03 13:48:48 -0400

    The work is completed done just need check

--- Main_code.py
+++ Main_code.py
@@ -1,7 +1,6 @@
 #this is could be the main terminal but guys feel free if you want to create more
 voter_id = [1,2,3,4,5,6,7,8]
 name = str()
-number_of_voter = len(voter_id) #this give the lengt of the list to know how many persons will vote
 age = int()
 democrats_votes = 0
 republicans_votes = 0 
@@ -15,9 +14,13 @@ def choose():
    global senators_vote
    global governor_vote
    global house_vote
+   print("-----------------------------------")
    print("You want to vote for the Senate press S")
+   print("-----------------------------------")
    print("You want to vote for the House press H")
+   print("-----------------------------------")
    print("You want to vote for the Governorship press G")
+   print("-----------------------------------")
    house = input("Your option: ")
    house = house.upper()
    if house == "S":
@@ -31,10 +34,12 @@ def choose():
       print("Thank you for your vote ", name)
    
 while True:
-   condition = input("Would you like to vote? (Yes or NO)")
+   print("Would you like to vote? (Yes or NO)")
+   condition = input()
    condition = condition.upper() 
    if condition == "YES":
     name = str(input("What's your name?"))
+    print("-----------------------------------")
     age = int(input("How old are you ?"))
     if age >= 18:
         voter = int(input("Enter your voter id: "))
@@ -52,22 +57,30 @@ while True:
             if choise == "R":
                republicans_votes = republicans_votes+1 #added republicans to add one per input
                print("Republican Votes: " + str(republicans_votes))#added to show republican votes
+               print("-----------------------------------")
                choose()
             elif choise == "D":
                democrats_votes = democrats_votes+1 #added democrats to add one per input
                print("Democrat Votes: " + str(democrats_votes))#added to show democrate votes
+               print("-----------------------------------")
                choose()
             elif choise == "O":
                others = others+1 #added others to add one per input
                print("Other Votes: " + str(others)) #added to show other votes
+               print("-----------------------------------")
                choose()
         else:
-            print("You have already vote!")
+           print("-----------------------------------")
+           print("You have already vote!")
+           print("-----------------------------------")
     else:
-         print("You need to be more 18 years old to vote!")
+      print("-----------------------------------") 
+      print("You need to be more 18 years old to vote!")
+      print("-----------------------------------")
    else:
     break
-   
+number_of_voter = len(voter_id) #this give the lengt of the list to know how many persons will vote
+print("-----------------------------------")   
 print("-----------------------------------")
 print("Total Voter: ", number_of_voter)
 print("-----------------------------------")
@@ -75,7 +88,8 @@ print("Democrats votes: ", democrats_votes)
 print("-----------------------------------")
 print("Republicans votes: ", republicans_votes)
 print("-----------------------------------")
-print(others)
+print("Others votes:", others)
+print("-----------------------------------")
 print("-----------------------------------")
 
 if democrats_votes > republicans_votes and democrats_votes > others:
@@ -85,7 +99,7 @@ elif republicans_votes > democrats_votes and republicans_votes > others: #Main s
 elif others > democrats_votes and others > republicans_votes:
    print("Others had won the elections!!!")
 else:
-   print("There is not a clear winner!")
+   print("There is not a clear winner!!")
 
 print("-----------------------------------")   
 print("-----------------------------------")
@@ -96,7 +110,3 @@ print("-----------------------------------")
 print("Governorship Votes: ", governor_vote)
 print("-----------------------------------")
 print("-----------------------------------")
-
-
-
-   
